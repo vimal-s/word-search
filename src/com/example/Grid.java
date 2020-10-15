2110d2eba1a59496aaa2fcda7db26c966e46c250
vimal <36556890+vimal-s@users.noreply.github.com>
2020-10-15 20:33:29 +0530

    Support word placement in multiple directions

--- src/com/example/Grid.java
+++ src/com/example/Grid.java
@@ -1,41 +1,83 @@
 package com.example;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Grid {
 
   private final int GRID_SIZE;
-  private final char[][] grid;
+  private final char[][] contents;
+
+  private enum Direction {
+    RIGHT,
+    DOWN,
+    BOTH
+  }
 
   public Grid(int gridSize) {
     this.GRID_SIZE = gridSize;
-    grid = new char[GRID_SIZE][GRID_SIZE];
+    contents = new char[GRID_SIZE][GRID_SIZE];
   }
 
   public void displayGrid() {
-    Arrays.stream(grid).map(Grid::charArrayToString).forEach(System.out::println);
+    Arrays.stream(contents).map(Grid::charArrayToString).forEach(System.out::println);
   }
 
-  public void placeWords(List<String> wordsToHide) {
-    wordsToHide.forEach(this::placeWord);
+  public void placeWords(List<String> words) {
+    words.forEach(this::placeWord);
   }
 
   public void placeWord(String word) {
+    if (word.length() > GRID_SIZE) {
+      return;
+    }
+
     int rowIndex;
     int colIndex;
 
+    List<Direction> directions = Arrays.asList(Direction.values());
+    Collections.shuffle(directions);
+
     // loop until you find the value is not yet assigned to this index
-    do {
+    boolean isNotAdded = true;
+    // todo: maintain array for neglected indexes and reset after loop is completed
+    //        else infinite loop may occur
+    while (isNotAdded) {
       rowIndex = ThreadLocalRandom.current().nextInt(GRID_SIZE);
       colIndex = ThreadLocalRandom.current().nextInt(GRID_SIZE);
-    } while (wordExceedsGrid(word, colIndex) || wordOverwrites(word, rowIndex, colIndex));
 
-    for (char curr : word.toCharArray()) {
-      grid[rowIndex][colIndex] = curr;
-      colIndex++;
+      for (Direction direction : directions) {
+        boolean testFailed = testCondition(word, colIndex, rowIndex, direction);
+
+        if (!testFailed) {
+          for (char curr : word.toCharArray()) {
+            contents[rowIndex][colIndex] = curr;
+
+            switch (direction) {
+              case RIGHT:
+                colIndex++;
+                break;
+              case DOWN:
+                rowIndex++;
+                break;
+              case BOTH:
+                rowIndex++;
+                colIndex++;
+                break;
+            }
+          }
+          isNotAdded = false;
+          break;
+        }
+      }
     }
+  }
+
+  private boolean testCondition(String word, int colIndex, int rowIndex, Direction direction) {
+    return wordExceedsGrid(word, rowIndex, colIndex, direction)
+        || wordOverwrites(word, rowIndex, colIndex, direction);
   }
 
   private static String charArrayToString(char[] chars) {
@@ -43,7 +85,7 @@ public class Grid {
 
     for (char value : chars) {
       if (value == '\u0000') {
-        sb.append('-');
+        sb.append('.');
       } else {
         sb.append(value);
       }
@@ -53,18 +95,47 @@ public class Grid {
     return sb.toString();
   }
 
-  private boolean wordExceedsGrid(String word, int colIndex) {
-    return colIndex + word.length() > GRID_SIZE;
+  // todo: what am I doing here?
+  private boolean wordExceedsGrid(String word, int rowIndex, int colIndex, Direction direction) {
+
+    int temp = 0;
+
+    switch (direction) {
+      case RIGHT:
+        temp = colIndex;
+        break;
+      case DOWN:
+        temp = rowIndex;
+        break;
+      case BOTH:
+        temp = GRID_SIZE - colIndex <= GRID_SIZE - rowIndex ? colIndex : rowIndex;
+        break;
+    }
+
+    return temp + word.length() > GRID_SIZE;
   }
 
-  private boolean wordOverwrites(String word, int rowIndex, int colIndex) {
+  private boolean wordOverwrites(String word, int rowIndex, int colIndex, Direction direction) {
     // maybe this loop is bad and should be replaced with a while loop
     for (char ignore : word.toCharArray()) {
-      if (grid[rowIndex][colIndex] != '\u0000') {
+      if (contents[rowIndex][colIndex] != '\u0000') {
         return true;
       }
-      colIndex++;
+
+      switch (direction) {
+        case RIGHT:
+          colIndex++;
+          break;
+        case DOWN:
+          rowIndex++;
+          break;
+        case BOTH:
+          rowIndex++;
+          colIndex++;
+          break;
+      }
     }
+
     return false;
   }
 }
